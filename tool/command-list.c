73dc1a6a166240c49bbca8330762d69095b9564c
Kay Sievers <kay@vrfy.org>
2017-08-06 19:34:14 +0200

    tool: list - add command to list methods

--- tool/command-list.c
+++ tool/command-list.c
@@ -8,7 +8,7 @@
 #include <getopt.h>
 #include <string.h>
 
-static long print_interfaces(VarlinkCli *cli, const char *field) {
+static long print_registry(VarlinkCli *cli, const char *field) {
         _cleanup_(varlink_object_unrefp) VarlinkObject *out = NULL;
         _cleanup_(freep) char *error = NULL;
         VarlinkArray *interfaces;
@@ -51,22 +51,80 @@ static long print_interfaces(VarlinkCli *cli, const char *field) {
         return 0;
 }
 
+static long print_methods(VarlinkCli *cli, const char *interface_name) {
+        _cleanup_(freep) char *address = NULL;
+        _cleanup_(varlink_object_unrefp) VarlinkObject *parameters = NULL;
+        _cleanup_(varlink_object_unrefp) VarlinkObject *reply = NULL;
+        _cleanup_(freep) char *error = NULL;
+        _cleanup_(varlink_interface_freep) VarlinkInterface *interface = NULL;
+        const char *interfacestring = NULL;
+        _cleanup_(freep) char *string = NULL;
+        long r;
+
+        r = varlink_cli_resolve(cli, interface_name, &address);
+        if (r < 0)
+                return r;
+
+        r = varlink_cli_connect(cli, address);
+        if (r < 0)
+                return r;
+
+        varlink_object_new(&parameters);
+        varlink_object_set_string(parameters, "name", interface_name);
+        r = varlink_cli_call(cli, "org.varlink.service.GetInterface", parameters, 0);
+        if (r < 0)
+                return r;
+
+        r = varlink_cli_wait_reply(cli, &reply, &error, NULL);
+        if (r < 0)
+                return r;
+
+        if (error) {
+                printf("Error: %s\n", error);
+
+                return 0;
+        }
+
+        if (varlink_object_get_string(reply, "interfacestring", &interfacestring) < 0)
+                return -CLI_ERROR_CALL_FAILED;
+
+        r = varlink_interface_new(&interface, interfacestring, NULL);
+        if (r < 0)
+                return -CLI_ERROR_PANIC;
+
+        for (unsigned long i = 0; i < interface->n_members; i += 1) {
+                const VarlinkInterfaceMember *member = &interface->members[i];
+
+                if (member->type != VARLINK_MEMBER_METHOD)
+                        continue;
+
+                printf("%s\n", member->name);
+        }
+
+        return 0;
+}
+
 static long list(VarlinkCli *cli) {
         static const struct option options[] = {
                 { "help",             no_argument, NULL, 'h' },
                 {}
         };
         const char *type = NULL;
+        const char *argument = NULL;
         int c;
 
         while ((c = getopt_long(cli->argc, cli->argv, "h", options, NULL)) >= 0) {
                 switch (c) {
                         case 'h':
-                                printf("Usage: %s list interfaces|addresses\n", program_invocation_short_name);
-                                printf("\n");
-                                printf("List available interfaces, addresses\n");
+                                printf("Usage: %s COMMAND [OPTIONS]\n", program_invocation_short_name);
                                 printf("\n");
                                 printf("  -h, --help             display this help text and exit\n");
+                                printf("\n");
+                                printf("Commands:\n");
+                                printf(" addresses                List all registered addresses\n");
+                                printf(" interfaces               List all registered interfaces\n");
+                                printf(" methods INTERFACE        List methods of specified interface\n");
+                                printf("\n");
                                 return EXIT_SUCCESS;
 
                         default:
@@ -76,15 +134,19 @@ static long list(VarlinkCli *cli) {
 
         type = cli->argv[optind];
         if (!type) {
-                fprintf(stderr, "Error: expecting addresses|interfaces\n");
+                fprintf(stderr, "Error: expecting command\n");
 
                 return CLI_ERROR_MISSING_ARGUMENT;
         }
 
+        argument = cli->argv[optind + 1];
+
         if (strcmp(type, "interfaces") == 0)
-                return print_interfaces(cli, "interface");
+                return print_registry(cli, "interface");
         else if (strcmp(type, "addresses") == 0)
-                return print_interfaces(cli, "address");
+                return print_registry(cli, "address");
+        else if (strcmp(type, "methods") == 0)
+                return print_methods(cli, argument);
 
         return exit_error(CLI_ERROR_INVALID_ARGUMENT);
 }
