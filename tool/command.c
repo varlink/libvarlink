807148f248084a07f52133fabb51fb66511541ef
Kay Sievers <kay@vrfy.org>
2017-09-25 14:45:11 +0200

    tool: Remove 'error' verb

--- tool/command.c
+++ tool/command.c
@@ -3,7 +3,6 @@
 extern const CliCommand command_bridge;
 extern const CliCommand command_call;
 extern const CliCommand command_complete;
-extern const CliCommand command_error;
 extern const CliCommand command_format;
 extern const CliCommand command_help;
 extern const CliCommand command_info;
@@ -13,7 +12,6 @@ const CliCommand *cli_commands[] = {
         &command_bridge,
         &command_call,
         &command_complete,
-        &command_error,
         &command_format,
         &command_help,
         &command_info,
