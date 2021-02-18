2ce64b3c6ccef85afc3c26f1ce74c4cbea70ef16
Harald Hoyer <harald@redhat.com>
2021-02-18 10:59:53 +0100

    feat: compile with CC=clang

--- varlink-wrapper.py
+++ varlink-wrapper.py
@@ -8,6 +8,9 @@ path_out = sys.argv[2]
 interface = os.path.basename(path_in).replace('.', '_')
 
 with open(path_out, 'wt') as output:
-    print('static const char *{} = R"INTERFACE('.format(interface), file=output)
-    print(open(path_in).read(), end='', file=output)
-    print(')INTERFACE";', file=output)
+    print('static const char {}[] = {{'.format(interface), file=output)
+    with open(path_in) as input:
+        for line in input:
+            for char in line:
+                print('{},'.format(ord(char)), file=output)
+    print('0 };', file=output)
