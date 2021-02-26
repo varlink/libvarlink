f10f19816345e5cca630388e244581e99a84cd28
Harald Hoyer <harald@redhat.com>
2021-02-26 13:34:12 +0100

    tests: add JSONTestSuite

--- lib/test-object.c
+++ lib/test-object.c
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
+#define _GNU_SOURCE
+
 #include "varlink.h"
+#include "util.h"
 
 #include <assert.h>
 #include <math.h>
 #include <string.h>
 #include <locale.h>
 #include <stdio.h>
+#include <glob.h>
+#include <errno.h>
 
 static void test_api(void) {
         VarlinkObject *s;
@@ -119,11 +124,113 @@ static void test_json(void) {
         assert(varlink_object_unref(s) == NULL);
 }
 
-int main(void) {
+static int read_file(FILE *file, char **contentsp) {
+        _cleanup_ (fclosep) FILE *stream = NULL;
+        _cleanup_ (freep) char *contents = NULL;
+        size_t n_input;
+
+        stream = open_memstream(&contents, &n_input);
+        if (!stream)
+                return -errno;
+
+        fputs("{ \"test\" : ", stream);
+
+        for (;;) {
+                char buffer[8192];
+                size_t n;
+
+                n = fread(buffer, 1, sizeof(buffer), file);
+                if (n == 0) {
+                        if (ferror(file))
+                                return -errno;
+
+                        break;
+                }
+
+                if (fwrite(buffer, 1, n, stream) != n)
+                        return -errno;
+        }
+
+        fputs("}", stream);
+        fclose(stream);
+        stream = NULL;
+
+        *contentsp = contents;
+        contents = NULL;
+
+        return 0;
+}
+
+static int test_json_test_suite(char *base_dir) {
+        _cleanup_(globfree) glob_t globbuf;
+        _cleanup_(freep) char *realsrc = NULL;
+
+        int j;
+        int r = 0;
+
+        // All n_*.json and i_*.json
+        assert(asprintf(&realsrc, "%s/[ni]_*.json", base_dir) >= 0);
+        assert(glob(realsrc, 0, NULL, &globbuf) == 0);
+        for (j = 0; j < globbuf.gl_pathc; j++) {
+                _cleanup_(freep) char *input = NULL;
+                _cleanup_(fclosep) FILE *file = NULL;
+                VarlinkObject *s;
+
+                file = fopen(globbuf.gl_pathv[j], "r");
+                assert(file != NULL);
+                assert(read_file(file, &input) == 0);
+                fprintf(stderr, "%s ... ", globbuf.gl_pathv[j]);
+                if (varlink_object_new_from_json(&s, input) != 0) {
+                        fprintf(stderr, "[OK]\n");
+                } else {
+                        fprintf(stderr, "[FAILED]\n");
+                        assert(varlink_object_unref(s) == NULL);
+                        r++;
+                }
+        }
+
+        free(realsrc);
+        realsrc = NULL;
+
+        globfree(&globbuf);
+        memset(&globbuf, 0, sizeof(globbuf));
+
+        // All y_*.json
+        assert(asprintf(&realsrc, "%s/y_*.json", base_dir) >= 0);
+        assert(glob(realsrc, 0, NULL, &globbuf) == 0);
+
+        for (j = 0; j < globbuf.gl_pathc; j++) {
+                _cleanup_(freep) char *input = NULL;
+                _cleanup_(fclosep) FILE *file = NULL;
+                VarlinkObject *s;
+
+                file = fopen(globbuf.gl_pathv[j], "r");
+                assert(file != NULL);
+                assert(read_file(file, &input) == 0);
+                fprintf(stderr, "%s ... ", globbuf.gl_pathv[j]);
+                if (varlink_object_new_from_json(&s, input) == 0) {
+                        fprintf(stderr, "[OK]\n");
+                        assert(varlink_object_unref(s) == NULL);
+                } else {
+                        fprintf(stderr, "[FAILED]\n");
+                        r++;
+                }
+        }
+
+        return r;
+}
+
+int main(int argc, char **argv) {
+
+        assert(argc != 1);
+
         // Uses `,` as the radix character
         assert(setlocale(LC_NUMERIC, "de_DE.UTF-8") != 0);
 
         test_api();
+
+        assert(test_json_test_suite(argv[1]) == 0);
+
         test_json();
 
         return EXIT_SUCCESS;
